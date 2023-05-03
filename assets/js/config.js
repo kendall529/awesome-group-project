cbf10c707b4cc8adcd22605aef046b043b206ee7
kendall529 <kendall.smith529@gmail.com>
2023-05-02 22:23:52 -0500

    set up some scoreboard code

--- assets/js/config.js
+++ assets/js/config.js
@@ -220,6 +220,8 @@ function createEventListener(j, resolve, questions, currentIndex) {
         // go to next question if there are more questions
         if (currentIndex + 1 < questions.length) {
             renderQuestion(questions, currentIndex + 1);
+        } else {
+            window.location.href = './scoreboard.html'
         }
     }
 }
